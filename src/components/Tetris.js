be88063fb6662173918c79982768b2f47d05cd8c
skeown13 <code@keown.net>
2020-07-10 17:19:52 -0700

    created two states of dropTime and gameOver. if Game Over display so otherwise display the Score, Rows, and Level. beginnings of using [player] and [stage, setStage]

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -13,18 +13,27 @@ import Display from './Display'
 import StartButton from './StartButton'
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null)
+  const [gameOver, setGameOver] = useState(false)
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player)
 
   console.log("re-render")
   
   return (
     <StyledTetrisWrapper>
-      <Stage stage={createStage()} />
+      <Stage stage={stage} />
       <aside>
-        <StyledTetris>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </StyledTetris>
+        {gameOver ? (
+          <Display gameOver={gameOver} text="Game Over" />
+        ) : (
+          <StyledTetris>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </StyledTetris>
+        )}
         <StartButton />
       </aside>
     </StyledTetrisWrapper>
