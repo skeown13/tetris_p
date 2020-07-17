e37c1a115d35d7b5705641c53b720cb4f8e9330e
skeown13 <code@keown.net>
2020-07-17 14:56:59 -0700

    updating player position and reseting player. also added in keypress buttons

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -18,33 +18,43 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null)
   const [gameOver, setGameOver] = useState(false)
 
-  const [player] = usePlayer();
-  const [stage, setStage] = useStage(player)
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer)
 
   console.log("re-render")
 
   const movePlayer = dir => {
-
+    updatePlayerPos( { x: dir, y: 0 })
   }
 
   const startGame = () => {
-
+    //Reset everything
+    setStage(createStage())
+    resetPlayer()
   }
-
+  
   const drop = () => {
-
+    updatePlayerPos({ x:0, y: 1, collided: false})
   }
 
   const dropPlayer = () => {
-
+    drop()
   }
 
   const move = ({ keyCode }) => {
-
+    if(!gameOver) {
+      if(keyCode === 37) {
+        movePlayer(-1)
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
   }
   
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <Stage stage={stage} />
       <aside>
         {gameOver ? (
@@ -56,7 +66,7 @@ const Tetris = () => {
             <Display text="Level" />
           </StyledTetris>
         )}
-        <StartButton />
+        <StartButton onClick={startGame}/>
       </aside>
     </StyledTetrisWrapper>
   )
